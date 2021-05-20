12625e75fb83545f149ddbc791cb60c66ca6f9f3
Constantin Antochi <constantin.antochi@gmail.com>
2021-05-20 19:46:59 +0300

    punctul 1 done

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ $(document).ready(function () {
   $.ajax({
     url: 'http://private-32dcc-products72.apiary-mock.com/product', success: function (result) {
       console.log(JSON.stringify(result[1], null, 4))
+      $('.order__listing__row').clone().insertAfter('.order__listing__row').attr('id', 'order__listing__row__4713663');
+      $('#order__listing__row__4713663 > .order__listing__item__name').text(result[1].name)
+      $('#order__listing__row__4713663 > .order__listing__item__unit__price').text(result[1].price + '$')
+      $('#order__listing__row__4713663 > .order__listing__item__total__price').text(result[1].price + '$')
     }
   })
+
+
+
 });
